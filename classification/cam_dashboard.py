3124b0b6074dbc18984c6d4b910d04b8949eb8fc
Rimo-rimo <wjdrbs51@koreatech.ac.kr>
2023-02-23 15:47:22 +0900

    ADD cam_dashboard.py

--- classification/cam_dashboard.py
+++ classification/cam_dashboard.py
@@ -21,6 +21,7 @@ from albumentations.pytorch import ToTensorV2
 
 from pycocotools.coco import COCO
 import json
+import time
 
 # ===========================parameter===========================
 model_folder = "/Users/rimo/Documents/paper/detector/classification/models/"
@@ -97,6 +98,16 @@ def basic_crop(img):
     img = torch.unsqueeze(img,0)
     return img
 
+class resnet18(nn.Module):
+    def __init__(self, num_classes):
+        super(resnet18, self).__init__()
+        self.net = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
+        self.net.fc = nn.Linear(in_features = 512, out_features=num_classes, bias=True)
+
+    def forward(self, x):
+        x = self.net(x)
+        return x
+
 class resnet50(nn.Module):
     def __init__(self, num_classes):
         super(resnet50, self).__init__()
@@ -106,17 +117,49 @@ class resnet50(nn.Module):
         x = self.net(x)
         return x
 
+class swinT(nn.Module):
+    def __init__(self, num_classes):
+        super(swinT, self).__init__()
+        self.net = torchvision.models.swin_t(weights='DEFAULT')
+        self.net.head = nn.Linear(in_features = 768, out_features=num_classes, bias=True)
+
+    def forward(self, x):
+        x = self.net(x)
+        return x
+
+class swinS(nn.Module):
+    def __init__(self, num_classes):
+        super(swinS, self).__init__()
+        self.net = torchvision.models.swin_s(weights='DEFAULT')
+        self.net.head = nn.Linear(in_features = 768, out_features=num_classes, bias=True)
+
+    def forward(self, x):
+        x = self.net(x)
+        return x
+
+class swinB(nn.Module):
+    def __init__(self, num_classes):
+        super(swinB, self).__init__()
+        self.net = torchvision.models.swin_b(weights='DEFAULT')
+        self.net.head = nn.Linear(in_features = 1024, out_features=num_classes, bias=True)
+
+    def forward(self, x):
+        x = self.net(x)
+        return x
+
 
 # ST_사이드바 구축
-st.title(':blue[CAM Dashboard]')
+# st.title('[CAM Dashboard]')
+st.markdown("<h1 style='text-align: center; color : #0067C8;'>CAM Dashboard</h1>", unsafe_allow_html=True)
+st.title(" ")
 with st.sidebar:
-    model_name = st.selectbox(":blue[___Select model name___]",model_names, index=0)
-    model_epoch = st.slider(":blue[___Select model epoch(*5)___]", min_value=1, max_value=4, value=4)
-    model_layer = st.slider(":blue[___Select layer for the CAM___]", min_value=1, max_value=10, value=10)
-    transform = st.selectbox(":blue[___Select transform___]", ["basic", "basic_crop", "noise_crop"], index=0)
-    data_class = st.radio(":blue[___Select data class___]", [0,1,2,3], format_func=lambda x:number_to_class[x], index=3)
+    model_name = st.selectbox(":blue[__MODEL NAME__]",model_names, index=0)
+    model_epoch = st.slider(":blue[__MODEL EPOCH(*5)__]", min_value=1, max_value=4, value=4)
+    model_layer = st.slider(":blue[__LAYER FOR CAM__]", min_value=1, max_value=4, value=4)
+    transform = st.selectbox(":blue[__TRANSFORM__]", ["basic", "basic_crop", "noise_crop"], index=0)
+    data_class = st.radio(":blue[__DATA CLASS__]", [0,1,2,3], format_func=lambda x:number_to_class[x], index=3)
     data = data[data["label"] == data_class]
-    data_index = st.number_input(":blue[___Select data index___]", min_value=0, max_value=len(data)-1, step=1, value=0)
+    data_index = st.number_input(":blue[__DATA INDEX__]", min_value=0, max_value=len(data)-1, step=1, value=0)
 
 # ===========================back===========================
 model_path = model_folder + model_name + f"/{model_epoch * 5}.pth"
@@ -124,12 +167,29 @@ model = globals()[model_name.split("/")[0]](4)
 model.load_state_dict(torch.load(model_path, map_location=device))
 model = model.eval()
 
+start = time.time()
 if transform in ["basic_crop", "noise_crop"]:
     w,h = 550, 240
 else:
     w,h = 600, 250
+if model_name.split("/")[0] == "resnet18":
+    target_layers = [model.net.layer4[-1].conv2]
+elif model_name.split("/")[0] == "resnet50":
+    if model_layer == 1:
+        target_layers = [model.net.layer1[-1].conv3]
+    elif model_layer == 2:
+        target_layers = [model.net.layer2[-1].conv3]
+    elif model_layer == 3:
+        target_layers = [model.net.layer3[-1].conv3]
+    elif model_layer == 4:
+        target_layers = [model.net.layer4[-1].conv3]
+elif model_name.split("/")[0] == "swinT":
+    target_layers = [model.net.features[-1][1].norm1]
+elif model_name.split("/")[0] == "swinS":
+    target_layers = [model.net.features[-1][1].norm1]
+elif model_name.split("/")[0] == "swinB":
+    target_layers = [model.net.features[-1][1].norm1]
 
-target_layers = [model.net.layer4[-1].conv3]
 cam = GradCAM(model=model, target_layers=target_layers, use_cuda=False)
 
 img_path = image_folder + data["image_path"].iloc[data_index]
@@ -152,13 +212,13 @@ visualization = cv2.resize(visualization, dsize=(620,250))
 
 image = cv2.resize(image, dsize=(620,250))
 print("Pred : ",number_to_class[pred])
-
+end = time.time()
 # ==================bounding box================
 img = cv2.imread("/Users/rimo/Documents/paper/data/images/"+data["image_path"].iloc[data_index])
 # print(int(data["image_id"].iloc[data_index]))
 anns_ids = coco.getAnnIds(imgIds=data["image_id"].iloc[data_index], iscrowd=None)
 anns = coco.loadAnns(anns_ids)
-print(anns)
+print("총 수행 시간 : ", end - start)
 for i in anns:
     bbox = i["bbox"]
     x1 = int(bbox[0])
@@ -166,21 +226,36 @@ for i in anns:
     x2 = x1 + int(bbox[2])
     y2 = y1 + int(bbox[3])
     img = cv2.rectangle(img,(x1,y1),(x2,y2),[255,255,0],10)
-
+print(img.shape)
+img = img[ 700:1500 , 50 :2300]
+img = cv2.resize(img, dsize=(850,300))
+st.markdown("<h4 style='text-align: center;'>[Origin Image]</h4>", unsafe_allow_html=True)
 st.image(img)
 
 # ST_image 시각화
-st.caption(f":blue[Model] -> {model_name}/{model_epoch*5}.pth")
-st.caption(f":blue[Transform] -> {transform}")
-data_path = data["image_path"].iloc[data_index]
-st.caption(f":blue[Data] -> {data_path}")
+# c1, c2, c3 = st.columns(3)
+# with c1:
+#     st.caption(f":blue[__[MODEL]__] {model_name}/{model_epoch*5}.pth")
+# with c2:
+#     st.caption(f":blue[[__TRANSFORM__]] {transform}")
+# with c3:
+#     data_path = data["image_path"].iloc[data_index]
+#     st.caption(f":blue[__[DATA]__] {data_path}")
+# st.caption(f":blue[Model] -> {model_name}/{model_epoch*5}.pth")
+# st.caption(f":blue[Transform] -> {transform}")
+# data_path = data["image_path"].iloc[data_index]
+# st.caption(f":blue[Data] -> {data_path}")
 col1, col2 = st.columns(2)
 with col1:
-    st.subheader("Origin Image")
+    st.markdown("<h4 style='text-align: center;'>[Knurling]</h4>", unsafe_allow_html=True)
+    # st.subheader("Origin Image")
     st.image(image)
-    st.text(gt)
+    st.text(f"[GT] : {gt}")
+    # st.markdown("<h10 style='text-align: center; color : #808080;'>[Origim Image]</h10>", unsafe_allow_html=True)
 
 with col2:
-    st.subheader("Camed Image")
+    st.markdown("<h4 style='text-align: center;'>[CAM Image]</h4>", unsafe_allow_html=True)
+    # st.subheader("Camed Image")
     st.image(visualization)
-    st.text(number_to_class[pred])
+    st.text(f"[Pred] : {number_to_class[pred]}")
+    # st.markdown("<h10 style='text-align: center; color : #808080;'>[Origim Image]</h10>", unsafe_allow_html=True)
